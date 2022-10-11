94d37ace7ccef7045d27dcb223d28ea2a293fa2c
augusstt06 <augusstt06@gmail.com>
2022-10-11 16:22:47 +0900

    Apply gitignore

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 0xA1;
+    int num2 = 012;
+
+    printf("16진수 0xA1의 10진수 정수 값 : %d \n", num1);
+    printf("8진수 012의 10진수 정수 값 : %d \n", num2);
+    // printf("5 * 4 - 10 = 10\n");
+    // int a=5, b=4, c=10;
+    // printf("%d * %d - %d = %d\n", a, b, c, a * b - c);
+    // a = a * b - c;
+    // printf ("a의 메모리 [%d] = %d\n", &a, a);
+
+    return 0;
+}
